1d5ac6b5078d33d972cfb2ccfac2267a62f84e65
Web-Mastermind <AZEAgent@gmail.com>
2024-03-15 01:27:19 +0400

    fixed class names

--- src/Components/Sidevar/Menu.tsx
+++ src/Components/Sidevar/Menu.tsx
@@ -24,14 +24,14 @@ const Menu = () => {
         <input type="checkbox" role="button" aria-label="Display the menu" className={burgerMenuOpen ? styles.menu : styles.hideMenu} onClick={() => toggleBurgerMenu()} />
       </div>
       <nav className={`${styles.navMenu} ${burgerMenuOpen ? styles.showMenu : ''}`}>
-        <li className={styles.navMenuItems}><Link className={styles.navLinks} onClick={() => setBurgerMenuOpen(false)} href="/">Home</Link></li>
-        <li className={styles.navMenuItems}><Link className={styles.navLinks} onClick={() => setBurgerMenuOpen(false)} href="/">About</Link></li>
-        <li className={styles.navMenuItems}><Link className={styles.navLinks} onClick={() => setBurgerMenuOpen(false)} href="/">Services</Link></li>
-        <li className={styles.navMenuItems}><Link className={styles.navLinks} onClick={() => setBurgerMenuOpen(false)} href="/">Contact</Link></li>
+        <li className={styles.navMenuItems}><Link className={styles.navMenuLinks} onClick={() => setBurgerMenuOpen(false)} href="/">Home</Link></li>
+        <li className={styles.navMenuItems}><Link className={styles.navMenuLinks} onClick={() => setBurgerMenuOpen(false)} href="/">About</Link></li>
+        <li className={styles.navMenuItems}><Link className={styles.navMenuLinks} onClick={() => setBurgerMenuOpen(false)} href="/">Services</Link></li>
+        <li className={styles.navMenuItems}><Link className={styles.navMenuLinks} onClick={() => setBurgerMenuOpen(false)} href="/">Contact</Link></li>
         <ul className={styles.ulList}>
-          <li className={styles.navMenuSocItems}><Link className={styles.navSocLinks} onClick={() => setBurgerMenuOpen(false)} target="_blank" href="https://www.facebook.com/"><Image className={`${styles.siteSocialIconss} ${styles.gap}`} src="/assets/icons/facebookIcon.svg" alt="Facebook Icon" width={15} height={15} /></Link></li>
-          <li className={styles.navMenuSocItems}><Link className={styles.navSocLinks} onClick={() => setBurgerMenuOpen(false)} target="_blank" href="https://twitter.com/?lang=en"><Image className={`${styles.siteSocialIcons} ${styles.gap}`} src="/assets/icons/twitterIcon.svg" alt="Twitter Icon" width={15} height={15} /></Link></li>
-          <li className={styles.navMenuSocItems}><Link className={styles.navSocLinks} onClick={() => setBurgerMenuOpen(false)} target="_blank" href="https://www.linkedin.com/"><Image className={`${styles.siteSocialIcons} ${styles.gap}`} src="/assets/icons/linkedinIcon.svg" alt="Linkedin Icon" width={15} height={15} /></Link></li>
+          <li className={styles.navMenuSocItems}><Link className={styles.navMenuSocLinks} onClick={() => setBurgerMenuOpen(false)} target="_blank" href="https://www.facebook.com/"><Image className={`${styles.siteSocialIconss} ${styles.gap}`} src="/assets/icons/facebookIcon.svg" alt="Facebook Icon" width={15} height={15} /></Link></li>
+          <li className={styles.navMenuSocItems}><Link className={styles.navMenuSocLinks} onClick={() => setBurgerMenuOpen(false)} target="_blank" href="https://twitter.com/?lang=en"><Image className={`${styles.siteSocialIcons} ${styles.gap}`} src="/assets/icons/twitterIcon.svg" alt="Twitter Icon" width={15} height={15} /></Link></li>
+          <li className={styles.navMenuSocItems}><Link className={styles.navMenuSocLinks} onClick={() => setBurgerMenuOpen(false)} target="_blank" href="https://www.linkedin.com/"><Image className={`${styles.siteSocialIcons} ${styles.gap}`} src="/assets/icons/linkedinIcon.svg" alt="Linkedin Icon" width={15} height={15} /></Link></li>
         </ul>
       </nav>
     </aside>
